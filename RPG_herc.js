590f54d392caaea0b65e30b054bd82e0fa6dde2e
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-08-30 11:14:26 -0500

    Narrative Adjustment and Bug Fix
    Adjusted the placement and sequence of Narrative prompts to happen in the correct order for the User. This should fix the bug where they were being prompted before receiving the description of the choices.

--- RPG_herc.js
+++ RPG_herc.js
@@ -164,7 +164,7 @@ function healthBar(enemyType, character){
     console.log(`${enemyType.name} Health:${enemyHealth}/???`);
     console.log(`Player Health:${characterHealth}/100`)
 }
-//Encounter Sequence Controller WIP
+//Encounter Sequence Controller
 function encounterInitiator(encounterCheck, type){
     if (encounterCheck === true){
         let enemyType = type;
@@ -207,7 +207,7 @@ function encounterInitiator(encounterCheck, type){
 ////Encounter Structure////
 
 
-//Narrative Selector WIP
+//Narrative Selector
 function narrative(section, part, choice){
     //I. Intro
     if(section === 1){
@@ -235,6 +235,7 @@ function narrative(section, part, choice){
             alert("As the sounds of battle become deathly silent. The light from the flames paint a silhouette of a clash that will be told for generations. Hercules standing tall above the still form of the Nemean Lion. One Trial down two to go. Continue your Journey.");
             alert("Wiping the blood from your brow you look towards the back of the chamber. There before you are three massive gates of wood and stone. One leads to your salvation, another to your next Trial, and the last will cause your downfall. Which shall you choose?");
         }
+        else if(part === 2){
             if(choice === 1){
                 alert("With a heave you push open the gate and make your way down a new path. As you go farther down, you start to notice deep gouges in the walls on both sides. You hear and feel a deep rumbling behind you. You try to quicken your pace before all light goes out and the boulder overtakes you. The last hint of your existence are the sounds of your bones shattering...");
             }
@@ -246,8 +247,9 @@ function narrative(section, part, choice){
             else if(choice === 3){
                 alert("With a heave you push open the gate and make your way down a new path. You notice as you continue down the path that the walls grow damp and what was once flat ground begins to go into a sharp decline. It's as if you are walking into the very depths of the Underworld. After what seems like hours, if not days, you stop before a gate lined with lanterns of green fire.");
                 alert("You pass through the gate, before a wave of moisture and foul odor hits you. You continue farther into the chamber as it expands to the point you can't see the walls or ceiling anymore through the darkness. You feel a hot breath on your neck before turning around looking into the maw of your next Trial, The Nine-Headed Lernaean Hydra. You leap back and brandish your weapon, ready for battle!");
-            } 
-        else if(part === 2){
+            }     
+        }     
+        else if(part === 3){
             alert("Game Over")
         } 
     }
@@ -257,6 +259,7 @@ function narrative(section, part, choice){
             alert("The Earth beneath your feet trembles as the last head of the Hydra is severed and the body collapses. You stand there gasping for breath covered in all manner of filth before starting to look for a way out of the chamber. This marks your second Trial finished with only one last battle before your Legend is complete.");
             alert("After searching the chamber twice over, you note that there are three ways to continue your Journey: a stairwell to your left and another to your right, and a ladder that leads further into the dark. Which shall you choose?");
         }
+        else if(part === 2){
             if(choice === 1){
                 alert("The echoes of your footsteps bounce through the stairwell as you make your way up. The stairwell seems to never end, minutes stretch into hours, hours stretch into what feels like days. You finally come to the realization that you have reached the bottom, even though you had been going up the whole time. You should be exhausted but instead you feel invigorated and your wounds seem to have never existed.");
                 alert("As you step out of the stairwell you find yourself on what would amount to a bridge made of stone. As you look down off the ledge of the bridge all you see is eternal darkness. You shift your gaze towards where the ceiling of the chamber should be but nothing but inky black meets your sight. Your steps don't even seem to echo anymore as you continue across the bridge. Uncertainty starts to seep into you.");
@@ -269,8 +272,9 @@ function narrative(section, part, choice){
             }
             else if(choice === 3){
                 alert("You step onto the ladder before slowly climbing down. Your muscles, aching from the previous battles, now straining to hold your weight as there is no bottom in sight but you continue to endure for a while longer. You finally make it to the bottom, where you find yourself in what seems to be a bed chamber? While dazed in confusion you hear a melodic humming coming from the bed that draws you in ever closer. What meets your eyes as you look up is the gaze of a beautiful woman. You manage one last step before all that is left of your Legend is cold stone.")
-            } 
-        else if(part === 2){
+            }
+        }     
+        else if(part === 3){
             alert("Game Over")
         }
     }
@@ -280,7 +284,7 @@ function narrative(section, part, choice){
             alert("As Cerberus manages one last howl before collapsing just before the Gates of Hades, You hold yourself up by strength of will alone. Battered, bloody, and beaten you remain standing while your enemies have fallen. Three Trials given, three Trials done, you slowly make your way back through and out of the Labyrinth head held high.")
         }
         else if(part === 2){
-            alert("Game Over.")
+            alert("Game Over.")//Place Holder for continuation
         }
     }
     //IIIII. Conclusion 
@@ -288,7 +292,7 @@ function narrative(section, part, choice){
         alert("Hercules returned to the King, with a trophy of irrefutable proof of each Trial. His Deeds and Legend to be recognized by mortals and Gods alike. The stories of Hercules will be told for generations to come, to never be lost by the sands of time.")
     }
 }
-//Game Sequence Initiator WIP
+//Game Sequence Initiator
 function runGame(){
     let encounterCheck = true;
     let gameContinue = true;
@@ -303,8 +307,9 @@ function runGame(){
     }
     gameContinue = encounterInitiator(encounterCheck, lion);
     if(gameContinue){
+        narrative(2, 1, 0);
         playerChoice = Number(prompt("1: First Door 2: Second Door 3: Third Door"));
-        narrative(2, 1, playerChoice);
+        narrative(2, 2, playerChoice);
         if(playerChoice === 1){
             encounterCheck = false;
             gameContinue = false;
@@ -314,8 +319,9 @@ function runGame(){
         }
         gameContinue = encounterInitiator(encounterCheck, hydra);
         if(gameContinue){
+            narrative(3, 1, 0);
             playerChoice = Number(prompt("1: left 2: right 3: ladder"));
-            narrative(3, 1, playerChoice);
+            narrative(3, 2, playerChoice);
             if(playerChoice === 1){
                 hercules.health = 100;
             }
@@ -329,11 +335,11 @@ function runGame(){
                 narrative(5, 0, 0);
             }
             else{
-                alert("Game Over")
+                narrative(3, 3, 0);
             }
         }
         else{
-            alert("Game Over")
+            narrative(2, 3, 0);
         } 
     }
     else{
